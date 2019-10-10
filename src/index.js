8fef0c4be545c5c87b4195d5d94556cf7401df21
Alfred Cepeda <alfredjcepeda@gmail.com>
2019-10-10 01:04:52 -0400

    Restores functionality of app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
-});
-
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
-
-/*import express from 'express';
-
-import config from './config';
+import express from 'express';
+import './config';
 import routes from './routes';
 import logger from './libs/logger';
 
@@ -49,4 +35,3 @@ logger.init('logs').then(() => {
   app.listen(port);
   logger.log(`Server started on ${port}`);
 });
-*/
